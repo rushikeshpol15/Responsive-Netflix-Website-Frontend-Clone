caab8214512a423a8e9fac48f26d23022ae43021
Rushikesh pol <98026195+rushikeshpol15@users.noreply.github.com>
2023-10-06 23:38:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-           <BrowserRouter basename={process.env.PUBLIC_URL} >
+           <HashRouter >
             <App />
-          </BrowserRouter>
+          </HashRouter>
   </React.StrictMode>
 );
 
