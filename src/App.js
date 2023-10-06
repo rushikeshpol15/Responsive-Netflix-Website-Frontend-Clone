d29df4016bdabfca1c1d1d1b46e61b797766a331
Rushikesh pol <98026195+rushikeshpol15@users.noreply.github.com>
2023-10-06 21:22:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
 
   return (
 
-         <HashRouter basename='/Responsive-Netflix-Website-Frontend-Clone/#' >
+         <BrowserRouter basename='/Responsive-Netflix-Website-Frontend-Clone/#' >
           <Routes>
             <Route path='/' element={<HomePage handleEmail={handleEmail}/>}/>
             <Route path='/Account' element={<Account handleEmail={handleEmail}/>} />
@@ -73,7 +73,7 @@ function App() {
 
 
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
 
 
 
