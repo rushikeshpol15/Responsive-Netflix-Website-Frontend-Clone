682075f1d6fe05440c7e76018d1df1574aee2d32
Rushikesh pol <98026195+rushikeshpol15@users.noreply.github.com>
2023-10-07 17:09:02 +0530

    Update FAQ.js

--- src/FAQ/FAQ.js
+++ src/FAQ/FAQ.js
@@ -1,9 +1,14 @@
 import HelpCenterFooter from "../HelpCenter/HelpCenterFooter";
 import HelpCenterNavbar from "../HelpCenter/HelpCenterNavbar";
 import FAQSection from "./FAQSection";
+import { useEffect } from "react";
+
 
 function FAQ()
 {
+     useEffect(()=>{
+        location.reload();
+    })
     return(
         <>
         <HelpCenterNavbar search={false}/>
