b977e8e465e8a432c39b1ea2a4b7e3889e12e6b2
Rushikesh pol <98026195+rushikeshpol15@users.noreply.github.com>
2023-10-08 04:18:29 +0530

    Update ForgotPasswordSection.js

--- src/ForgotPassword/ForgotPasswordSection.js
+++ src/ForgotPassword/ForgotPasswordSection.js
@@ -20,14 +20,14 @@ function ForgotPasswordSection()
 
                     <button className="forgot-pass-email-btn text-white">Email Me</button>
 
-                    <a href="#" className="I-cant-remember-a-tag text-decoration-none">I can't remember my email address or phone number.</a>
+                    <a  className="I-cant-remember-a-tag text-decoration-none">I can't remember my email address or phone number.</a>
             </div>
            
             <div className="reCAPTCHA-box">
 
                 <p className="text-secondary" style={{fontSize:"0.9em"}}>This page is protected by Google reCAPTCHA to ensure you're not a bot.{(learnMoreViSibility)?<span className="text-decoration-underline" style={{cursor:"pointer"}} onClick={()=>{setlearnMoreViSibility(false)}}> Learn more.</span>:null}</p>
                 
-                {(!learnMoreViSibility)?<p className="text-secondary" style={{fontSize:"0.9em"}} >The information collected by Google reCAPTCHA is subject to the Google <a href="/Privacy" className="text-decoration-none captcha-a-link fw-bold" style={{fontSize:"inherit"}}>Privacy Policy</a> and <a href="/TermsOfUse" className="text-decoration-none captcha-a-link fw-bold" style={{fontSize:"inherit"}}>Terms of Service</a>, and is used for providing, maintaining, and improving the reCAPTCHA service and for general security purposes (it is not used for personalised advertising by Google).</p>
+                {(!learnMoreViSibility)?<p className="text-secondary" style={{fontSize:"0.9em"}} >The information collected by Google reCAPTCHA is subject to the Google <a onClick={()=>{Navigate('/Privacy')}} className="text-decoration-none captcha-a-link fw-bold" style={{fontSize:"inherit"}}>Privacy Policy</a> and <a onClick={()=>{Navigate('/TermsOfUse')}} className="text-decoration-none captcha-a-link fw-bold" style={{fontSize:"inherit"}}>Terms of Service</a>, and is used for providing, maintaining, and improving the reCAPTCHA service and for general security purposes (it is not used for personalised advertising by Google).</p>
                 :null}
 
             </div>
