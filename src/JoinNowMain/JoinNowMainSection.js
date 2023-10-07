64e859c35f3e5b4c2611d45c14d87cf0e9736a84
Rushikesh pol <98026195+rushikeshpol15@users.noreply.github.com>
2023-10-07 15:53:46 +0530

    Update JoinNowMainSection.js

--- src/JoinNowMain/JoinNowMainSection.js
+++ src/JoinNowMain/JoinNowMainSection.js
@@ -1,7 +1,10 @@
 import "./JoinNowMain.css";
+import { useNavigate } from "react-router-dom";
 
 function JoinNowMainSection()
 {
+        let Navigate=useNavigate();
+
     return(
         <>
         <section className="join-now-main-section">
@@ -11,7 +14,7 @@ function JoinNowMainSection()
                 <span style={{fontSize:"0.8em"}} className="d-block text-center mb-2">STEP <strong>1</strong> OF <strong>3</strong></span>
                 <h2 className="fw-bold mb-3 text-center" style={{fontSize:"1.7em"}}>Finish setting up your account</h2>
                 <p className="text-center" style={{fontSize:"1.1em"}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Netflix is personalised for you. <br/> Create a password to watch on any device at any time.</p>
-                <a href="/JoinNowOne" className="join-now-main-section-last-btn text-decoration-none text-white py-3 mt-4 d-block  text-center" >Next</a>
+                <a onClick={()=>{Navigate('/JoinNowOne')}} className="join-now-main-section-last-btn text-decoration-none text-white py-3 mt-4 d-block  text-center" >Next</a>
 
             </div>
 
