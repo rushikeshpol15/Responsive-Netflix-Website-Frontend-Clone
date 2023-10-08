889722412d17f1e2e18d46b086f12df447902030
Rushikesh pol <98026195+rushikeshpol15@users.noreply.github.com>
2023-10-08 16:02:27 +0530

    Update TermsOfUseSectionPrint.js

--- src/TermsOfUse/TermsOfUseSectionPrint.js
+++ src/TermsOfUse/TermsOfUseSectionPrint.js
@@ -7,7 +7,7 @@ function TermsOfUseSectionPrint()
     return(
         <>
         <div className="d-flex justify-content-between mb-3">
-            <a href="/" className="text-danger text-decoration-none d-flex align-items-center print-a"> <FontAwesomeIcon icon={faArrowLeft} style={{color: "#df1111", marginRight:"1em"}} />Back to Help Home</a>
+            <a href="#" className="text-danger text-decoration-none d-flex align-items-center print-a"> <FontAwesomeIcon icon={faArrowLeft} style={{color: "#df1111", marginRight:"1em"}} />Back to Help Home</a>
             <button className="btn btn-white fw-bold border border-dark print-button">
             <FontAwesomeIcon icon={faPrint} style={{color: "#000000",marginRight:"0.5em"}} />
             <span className="print-text">PRINT</span>
